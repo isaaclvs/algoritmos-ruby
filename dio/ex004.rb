12f92255e8a3ce1482595c1b6df76ec4edf5b76f
Isaac <isaacfelipee3@gmail.com>
2024-03-09 16:14:27 -0300

    .

---
+++ dio/ex004.rb
@@ -0,0 +1,11 @@
+# Faça um algoritmo que receba um número inteiro 
+# e imprima na tela o seu antecessor e o seu sucessor.
+
+
+def resposta(num)
+  puts "Número = #{num}"
+  puts "Sucessor = #{num+1}"
+  puts "Antecessor = #{num-1}"
+end
+
+resposta(6)
